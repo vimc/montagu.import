280e053db3b8e259205863cb9cb7edd6430a77d2
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-01-24 16:47:44 +0000

    WIP

---
+++ script.R
@@ -0,0 +1,45 @@
+## We need the dropbox token
+source("dropbox.R")
+source("util.R")
+dropbox_login()
+
+devtools::load_all("~/Documents/Projects/epi/vimc/montagu-r")
+montagu::montagu_set_default_location("uat")
+montagu::montagu_authorise("test.user@imperial.ac.uk", "password")
+
+dat <- read_csv("dropbox_stochastic.csv")
+d <- as.list(dat[1, ])
+
+montagu_burden_estimates(d$group, d$touchstone,
+                         d$scenario)
+
+
+## First grab the certificate:
+cert <- download_certificate(d)
+
+path <- sprintf("File requests/%s", d$path)
+info <- rdrop2::drop_dir(path)
+
+id <- montagu_burden_estimate_set_create(d$group,
+                                         d$touchstone,
+                                         d$scenario,
+                                         "stochastic",
+                                         cert$id)
+
+filename <- download_estimate(d, 1L)
+
+## This fails with 400:
+## unknown-run-id: Unknown run ID with id '1'. Attempting to match against run parameter set null
+montagu_burden_estimate_set_upload(d$group,
+                                   d$touchstone,
+                                   d$scenario,
+                                   id,
+                                   filename,
+                                   TRUE)
+
+## And this fails with 403:
+## forbidden: You do not have sufficient permissions to access this resource. Missing these permissions: modelling-group:modelling_group_id/estimates.write
+montagu_burden_estimate_set_clear(d$group,
+                                  d$touchstone,
+                                  d$scenario,
+                                  id)
