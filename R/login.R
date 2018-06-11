3a05ee8f42207807497c05973add1133dc1a304b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-06-11 11:03:30 +0100

    Correct login details

--- R/login.R
+++ R/login.R
@@ -12,11 +12,12 @@ montagu_login <- function(name) {
   } else {
     stop(sprintf("Unknown montagu server '%s'", name))
   }
-  username <- "import"
+  username <- "kimwoodruff80@gmail.com"
   password <- vault$read("/secret/import/montagu", field = "password")
   server <- montagu::montagu_server(name, host, port = port,
                                     username = username, password = password,
                                     global = TRUE, overwrite = TRUE)
+  server$authorise()
   montagu::montagu_server_global_default_set(server)
   message(sprintf("logged onto montagu server '%s'", name))
 }
